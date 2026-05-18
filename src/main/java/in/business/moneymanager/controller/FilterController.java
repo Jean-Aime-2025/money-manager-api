0f74552ba2c93a35639e73caa8398ec6158cd1bf
Jean-Aime-2024 <jeanaime1000@gmail.com>
2026-05-18 18:46:58 +0200

    email and excel services

--- src/main/java/in/business/moneymanager/controller/FilterController.java
+++ src/main/java/in/business/moneymanager/controller/FilterController.java
@@ -29,7 +29,7 @@ public class FilterController {
         LocalDate startDate = filter.getStartDate() != null ? filter.getStartDate() : LocalDate.MIN;
         LocalDate endDate = filter.getEndDate() != null ? filter.getEndDate() : LocalDate.now();
         String keyword = filter.getKeyword() !=null ? filter.getKeyword() : "";
-        String sortField = filter.getSortField() != null ? filter.getSortOrder() : "date";
+        String sortField = filter.getSortField() != null ? filter.getSortField() : "date";
         Sort.Direction direction = "desc".equalsIgnoreCase(filter.getSortOrder()) ? Sort.Direction.DESC : Sort.Direction.ASC;
         Sort sort = Sort.by(direction,sortField);
 
