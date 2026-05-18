28a2ea367cb507ea191c92aac9c86338b7588311
Jean-Aime-2024 <jeanaime1000@gmail.com>
2026-05-18 18:56:15 +0200

    email and excel services

--- src/main/java/in/business/moneymanager/controller/FilterController.java
+++ src/main/java/in/business/moneymanager/controller/FilterController.java
@@ -26,21 +26,59 @@ public class FilterController {
 
     @PostMapping
     public ResponseEntity<?> filterTransaction(@RequestBody FilterDTO filter){
-        LocalDate startDate = filter.getStartDate() != null ? filter.getStartDate() : LocalDate.MIN;
-        LocalDate endDate = filter.getEndDate() != null ? filter.getEndDate() : LocalDate.now();
-        String keyword = filter.getKeyword() !=null ? filter.getKeyword() : "";
-        String sortField = filter.getSortField() != null ? filter.getSortField() : "date";
-        Sort.Direction direction = "desc".equalsIgnoreCase(filter.getSortOrder()) ? Sort.Direction.DESC : Sort.Direction.ASC;
-        Sort sort = Sort.by(direction,sortField);
 
-        if("income".equalsIgnoreCase(filter.getType())){
-            List<IncomeDTO> incomes = incomeService.filterIncomes(startDate,endDate,keyword,sort);
+        LocalDate startDate = filter.getStartDate() != null
+                ? filter.getStartDate()
+                : LocalDate.MIN;
+
+        LocalDate endDate = filter.getEndDate() != null
+                ? filter.getEndDate()
+                : LocalDate.now();
+
+        String keyword = filter.getKeyword() != null
+                ? filter.getKeyword()
+                : "";
+
+        String sortField = filter.getSortField() != null
+                ? filter.getSortField()
+                : "date";
+
+        Sort.Direction direction =
+                "desc".equalsIgnoreCase(filter.getSortOrder())
+                        ? Sort.Direction.DESC
+                        : Sort.Direction.ASC;
+
+        Sort sort = Sort.by(direction, sortField);
+
+        if ("income".equalsIgnoreCase(filter.getType())) {
+
+            List<IncomeDTO> incomes =
+                    incomeService.filterIncomes(
+                            startDate,
+                            endDate,
+                            keyword,
+                            sort
+                    );
+
             return ResponseEntity.ok(incomes);
+
         } else if ("expense".equalsIgnoreCase(filter.getType())) {
-            List<ExpenseDTO> expenses = expenseService.filterExpenses(startDate,endDate,keyword,sort);
+
+            List<ExpenseDTO> expenses =
+                    expenseService.filterExpenses(
+                            startDate,
+                            endDate,
+                            keyword,
+                            sort
+                    );
+
             return ResponseEntity.ok(expenses);
+
         } else {
-            return ResponseEntity.badRequest().body("Invalid type. Must be 'income' or 'expense'");
+
+            return ResponseEntity
+                    .badRequest()
+                    .body("Invalid type. Must be 'income' or 'expense'");
         }
     }
 }
